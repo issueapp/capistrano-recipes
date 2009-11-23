13282eb6e040b752430cd50987b4d1336193ccdc
speedmax <subjective@gmail.com>
2009-11-24 02:43:27 +1100

    Initial import

---
+++ recipes/remote.rb
@@ -0,0 +1,27 @@
+set :sudo_call, ''
+desc 'makes remote/rake calls to be executed with sudo'
+task :use_sudo do
+  set :sudo_call, 'sudo'
+end
+
+desc 'run rake task'
+task :rake do
+  ARGV.values_at(Range.new(ARGV.index('rake')+1,-1)).each do |task|
+    run "cd #{current_path}; #{sudo_call} RAILS_ENV=production rake #{task}"
+  end
+  exit(0)
+end
+
+desc 'run remote command'
+task :remote do
+  command=ARGV.values_at(Range.new(ARGV.index('remote')+1,-1))
+  run "cd #{current_path}; #{sudo_call} RAILS_ENV=production #{command*' '}"
+  exit(0)
+end
+
+desc 'run specified rails code on server'
+task :runner do
+  command=ARGV.values_at(Range.new(ARGV.index('runner')+1,-1))
+  run "cd #{current_path}; RAILS_ENV=production script/runner '#{command*' '}'"
+  exit(0)
+end
